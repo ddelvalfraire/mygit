c4754bf13540dd545f603b381d4f8ec325b86d25
David Del Val <ddelvalfraire@outlook.com>
2024-11-28 20:34:27 -0800

    feat: create event loop for development; structure basic commands

--- src/main.c
+++ src/main.c
@@ -1,6 +1,65 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+void clear_terminal();
+
+int main(void)
+{
+    char command[256];
+    printf("vcs started\n");
+    while (1)
+    {
+        printf("vcs> ");
+        scanf("%s", command);
+
+        if (strcmp(command, "exit") == 0)
+        {
+            printf("vcs ended\n");
+            break;
+        }
+        else if (strcmp(command, "init") == 0)
+        {
+            printf("vcs initialized\n");
+        }
+        else if (strcmp(command, "add") == 0)
+        {
+            printf("added\n");
+        }
+        else if (strcmp(command, "commit") == 0)
+        {
+            printf("committed\n");
+        }
+        else if (strcmp(command, "status") == 0)
+        {
+            printf("status\n");
+        }
+        else if (strcmp(command, "log") == 0)
+        {
+            printf("log\n");
+        }
+        else if (strcmp(command, "checkout") == 0)
+        {
+            printf("checkout\n");
+        }
+        else if (strcmp(command, "clear") == 0)
+        {
+            clear_terminal();
+        }
+        else
+        {
+            printf("unknown command\n");
+        }
+    }
     return 0;
+}
+
+void clear_terminal()
+{
+// Use system commands to clear the terminal
+#ifdef _WIN32
+    system("cls"); // Windows
+#else
+    system("clear"); // Unix/Linux/Mac
+#endif
 }
