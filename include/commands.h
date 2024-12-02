ae6b67715b7da323dd70ef1dd2865c96b96e8244
David Del Val <ddelvalfraire@outlook.com>
2024-12-01 20:43:35 -0800

    chore: remove unused files

--- include/commands.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-int command_init_validate(struct command *self, int argc, char **argv);
-int command_init_run(struct command *self, int argc, char **argv);
-void command_init_cleanup(struct command *self);
-
-#endif // COMMANDS_H
